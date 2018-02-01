c18950adf3606df08b48aff0764b6fed000cbb00
yinjinghao211 <1042382584@qq.com>
2018-02-02 00:01:29 +0800

    lx

--- SignInSystem/src/main/java/com/signInSystem/shiro/realms/ShiroRealm.java
+++ SignInSystem/src/main/java/com/signInSystem/shiro/realms/ShiroRealm.java
@@ -1,5 +1,6 @@
 package com.signInSystem.shiro.realms;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,13 +17,15 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.realm.AuthenticatingRealm;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 
 import javax.annotation.Resource;
 
-public class ShiroRealm extends AuthorizingRealm{
+public class ShiroRealm extends AuthenticatingRealm {
+
     @Resource
     private IUserService userService;
 
@@ -31,90 +34,39 @@ public class ShiroRealm extends AuthorizingRealm{
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        System.out.println("[ShiroRealm]   doGetAuthenticationInfo");
-        // TODO Auto-generated method stub
-        //System.out.println("doGetAuthenticationInfo"+token.hashCode());
-        //1.把AuthenticationToken转化成UsernamePasswordToken
-        UsernamePasswordToken upt=(UsernamePasswordToken) token;
-        //2.从UsernamePasswordToken 中获取userName
-        String userPhone=upt.getUsername();
-        Users user=this.userService.selectUser(userPhone);
-        // System.out.println("----------------->"+user);
+        System.out.println("ShiroRealm    进入认证系统");
+        UsernamePasswordToken upt= (UsernamePasswordToken) token;
+        String curUserPhone=upt.getUsername();
+        char [] curUserPassword1=upt.getPassword();
 
-        int userId=this.userService.selectUserId(userPhone);
-        //取到的是数据库中储存的密码
-        String userPwd=this.passwordService.selectUserPassword(userId);
-        // System.out.println("------------------>"+userId);
+        //拿到原始密码
+        String curUserPassword=String.valueOf(upt.getPassword());
 
-        System.out.println(userPhone+"---------"+userPwd);
+        //加密，和原密码表中的数据进行比对
 
-
-        String userPassword=upt.getPassword().toString();
-        //3.调用数据库的方法，从数据库中 查询userName 对应的用户记录
-        System.out.println("从数据库中获取"+userPhone+"---------------"+ userPassword +"所对应 的用户信息。");
-        //6.根据用户的情况来构建 AuthenticationInfo的对象并返回，通常使用的实现类是
-
-        //以下信息是从数据库中获取的
-        //principal ：认证的实体信息	  可以是userName也可以是数据表对应的用户的实体类对象
-        Object principal=userPhone;
-        //credentials:密码        //经md5加密1024 后的密码值
-        Object credentials=null;
-
-        if("admin".equals(userPhone)){
-            credentials="038bdaf98f2037b31f1e75b5b4c9b26e";
-        }else if("user".equals(userPhone)){
-            credentials="098d2c478e9c11555ce2823231e02ec1";
-        }
-
-        //realmName:当前对象的name调用父类的getName方法
-        String realmName=getName();
-        //盐值：
-        ByteSource credentialsSalt=ByteSource.Util.bytes(userPhone);
-
-
-
-
-        //SimpleAuthenticationInfo info=new SimpleAuthenticationInfo(principal, credentials, realmName);
-        SimpleAuthenticationInfo info=null;
-        info=new SimpleAuthenticationInfo(principal, credentials,credentialsSalt,realmName );
-
-
-
-        return info;
-    }
-
-    public static void main(String[] args) {
         String algorithmName="MD5";
-        Object source="123456";
+        Object source=curUserPassword;
         Object salt=ByteSource.Util.bytes("user");
         int hashIterations=1024;
 
-        Object result=new SimpleHash(algorithmName, source, salt, hashIterations);
-        System.out.println(result);
-    }
-    //授权会被shiro回调的方法
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        Object afterEncryptionPwd=new SimpleHash(algorithmName, source, salt, hashIterations);
 
-        //System.out.println("AuthorizingRealm!");
-        //1.从PrincipalCollection获取登录信息
-        //获取Realm信息时按配置顺序读取
-
-        //记住顺序！！！！！！！！！！！
-        Object principal=principals.getPrimaryPrincipal();
-
-        //2.利用登录用户的信息来获取当前用户的角色和权限(可能需要查询数据库)
-
-        Set<String> roles=new HashSet<String>();
-        roles.add("user");
-        if("admin".equals(principal)){
-            //admin可以访问两个页面！！！！！！！！！！
-            roles.add("admin");
+        int userId=this.userService.selectUserId(curUserPhone);
+        // System.out.println("------------------>"+userId);
+        //通过userid找密码
+        //System.out.println(this.passwordService.selectUserPassword(userId));
+        //此时passwords表中的密码
+        String userPassword=this.passwordService.selectUserPassword(userId);
+        System.out.println(userPassword+"---------"+curUserPhone);
+        SimpleAuthenticationInfo info=null;
+        if(userPassword==afterEncryptionPwd)
+        {
+            Object principal=curUserPhone;
+            Object credentials=userPassword;
+            ByteSource credentialsSalt=ByteSource.Util.bytes("user");
+            info=new SimpleAuthenticationInfo(principal, credentials,credentialsSalt,"user" );
+            //     info=new SimpleAuthenticationInfo(principal, credentials,credentialsSalt,realmName);
         }
-        //3.创建SimpleAuthorizationInfo接口，并设置roles属性
-
-        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo(roles);
-        //4.返回对象
         return info;
     }
 }
--- SignInSystem/src/main/java/com/signInSystem/xyz/controller/ShiroController.java
+++ SignInSystem/src/main/java/com/signInSystem/xyz/controller/ShiroController.java
@@ -1,12 +1,22 @@
 package com.signInSystem.xyz.controller;
 
+import com.signInSystem.xyz.model.Users;
+import com.signInSystem.xyz.service.IPasswordService;
+import com.signInSystem.xyz.service.IUserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ByteSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 
 /**
@@ -16,25 +26,55 @@ import org.springframework.web.bind.annotation.RequestParam;
     @RequestMapping("/shiro")
     public class ShiroController {
 
+        //在此处直接把密码加密，直接进行比较
 
-        @RequestMapping("/login")
+        @Resource
+        private IUserService userService;
+
+        @Resource
+        private IPasswordService passwordService;
+
+
+    @RequestMapping("/login")
+    @ResponseBody
         public String login(@RequestParam("userName") String userName,@RequestParam("userPwd") String userPwd) {
-            Subject currentUser = SecurityUtils.getSubject();
-                //测试是否已经被认证，，，就是已经登录
-            String result="redirect:/error.jsp";
-                //可以不必认证，直接select数据库中是否有这个值，没有就跳到别的界面
-                //把用户名和密码封装为UsernamePasswordToken对象
-                UsernamePasswordToken token = new UsernamePasswordToken(userName, userPwd);
-                //RememberMe
-                token.setRememberMe(true);
-                try {
-                    //执行登录
-                    currentUser.login(token);
-                    result="redirect:/list.jsp";
-                } catch (AuthenticationException ae) {
-                    System.out.println("登录失败---------------" + ae.getMessage());
-                }
 
-            return result;
+        //UsernamePasswordToken token = new UsernamePasswordToken(userName, userPwd);
+        //token.setRememberMe(true);
+        String result=null;
+        //拿到原始密码-----userPwd
+        //加密，和原密码表中的数据进行比对
+        String algorithmName="MD5";
+        Object source=userPwd;
+        Object salt= ByteSource.Util.bytes("user");
+        int hashIterations=1024;
+
+        Object afterEncryptionPwd=new SimpleHash(algorithmName, source, salt, hashIterations);
+        //通过phone找id
+        int userId=this.userService.selectUserId(userName);
+        //通过phone找对象
+        Users user=this.userService.selectUser(userName);
+        // System.out.println("------------------>"+userId);
+        //通过userid找密码
+        //System.out.println(this.passwordService.selectUserPassword(userId));
+        //此时passwords表中的密码
+        String userPassword=this.passwordService.selectUserPassword(userId);
+        System.out.println(afterEncryptionPwd+"---------"+userName);
+        //SimpleAuthenticationInfo info=null;
+        if(user!=null && afterEncryptionPwd.equals(userPassword)){
+            //System.out.println(user);
+            result="index";
+        }else{
+            result="error";
+        }
+//            try {
+//                //执行登录
+//                System.out.println("1."+token.hashCode());
+//
+//            }catch (AuthenticationException ae) {
+//                //unexpected condition?  error?
+//                System.out.println("登录失败"+ae.getMessage());
+//            }
+            return "0";
         }
 }
