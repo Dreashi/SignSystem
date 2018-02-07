ffcf3182aa5d4c39a96918baa89e63900f9d5ac8
Wangzunpeng123 <976392383@qq.com>
2018-02-07 17:59:21 +0800

    s

--- SignInSystem/src/main/java/com/signInSystem/xyz/service/IPasswordService.java
+++
@@ -1,9 +0,0 @@
-package com.signInSystem.xyz.service;
-
-import com.signInSystem.xyz.model.UserPassword;
-import com.signInSystem.xyz.model.Users;
-
-public interface IPasswordService {
-    public void insertPasswords(UserPassword userPassword);
-    public String selectTrueUserPassword(int user_id);
-}
